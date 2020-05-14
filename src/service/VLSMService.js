f61b0acc832788adbd635118c039c9302d09b6c3
Tanvir Sojal <tanvirahmedsojal@gmail.com>
2020-05-14 23:24:15 +0600

    proxy fix attempt

--- src/service/VLSMService.js
+++ src/service/VLSMService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 class VLSMService{
     constructor() {
-        this.allocationURL = "https://vlsm-calculator.herokuapp.com/api/v1/vlsm-calculator/allocation";
+        this.allocationURL = "/allocation";
     }
     async getHostGroupAllocation(request){
         // console.log("Query: ", request);
