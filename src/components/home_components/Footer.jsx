4dd1d3eb89b96c003469408f18cdeee6d44844b1
Tanvir Sojal <tanvirahmedsojal@gmail.com>
2020-05-23 15:20:53 +0600

    added space

--- src/components/home_components/Footer.jsx
+++ src/components/home_components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="row align-items-end pt-3 pb-3">
                 <div className="col-md-6 offset-md-3">
                     <div className="text-center footer">
-                        Developed by <a className="profile" target="_blank" rel="noopener noreferrer" href="https://github.com/ar-pavel">Pavel</a> and <a className="profile" target="_blank" rel="noopener noreferrer" href="https://github.com/tanvirsojal">Sojal</a>🔥
+                        Developed by <a className="profile" target="_blank" rel="noopener noreferrer" href="https://github.com/ar-pavel">Pavel</a> and <a className="profile" target="_blank" rel="noopener noreferrer" href="https://github.com/tanvirsojal">Sojal </a>🔥
                     </div>
                 </div>
             </div>
